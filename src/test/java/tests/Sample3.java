dfd43aee338e3d35424fbb35bfcc6cf8af051991
deepika12341234 <deepika.avula019@gmail.com>
2025-03-04 11:49:00 +0530

    'added-sample3-class-file'

---
+++ src/test/java/tests/Sample3.java
@@ -0,0 +1,20 @@
+package tests;
+
+public class Sample3 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		test1();
+		test2();
+
+	}
+
+	public static void test1() {
+		System.out.println("test1");
+	}
+
+	public static void test2() {
+		System.out.println("test2");
+	}
+
+}
